7e31e5b5b4cb21637da1881601783989db9f7ef7
maikel-bakker <maikel.bakker1991@gmail.com>
2018-12-02 12:55:22 +0100

    [FEATURE] Ingredient reducer and component

---
+++ src/actions/ingredientActions.js
@@ -0,0 +1,49 @@
+
+import IngredientService from '../services/ingredientService';
+
+export const fetchIngredients = () => {
+    return (dispatch) => {
+        dispatch({
+            type: 'FETCH_INGREDIENTS_PENDING'
+        });
+
+        IngredientService.getIngredients()
+        .then(data => {
+            dispatch({
+                type: 'FETCH_INGREDIENTS_FULFILLED',
+                payload: data
+            })
+
+        })
+        .catch(error => {
+            dispatch({
+                type: 'FETCH_INGREDIENTS_REJECTED',
+                payload: error
+            })
+        });
+    }
+}
+
+export const addIngredient = (ingredient) => {
+    return (dispatch) => {
+        dispatch({
+            type: 'ADD_INGREDIENT_PENDING'
+        });
+
+        IngredientService.addIngredient(ingredient)
+        .then(data => {
+            console.log(data);
+            dispatch({
+                type: 'ADD_INGREDIENT_FULFILLED',
+                payload: data
+            })
+
+        })
+        .catch(error => {
+            dispatch({
+                type: 'ADD_INGREDIENT_REJECTED',
+                payload: error
+            })
+        });
+    }
+}
---
+++ src/reducers/ingredientsReducer.js
@@ -0,0 +1,85 @@
+const initialState = {
+    ingredients: [],
+    fetching: false,
+    fetched: false
+}
+
+const checkIfKeyExists = (key, arr) => {
+    if (arr.length) {
+        arr.forEach(el => {
+            return (el._id === key);
+        })
+    } else {
+        return true;   
+    }
+
+}
+
+export default function ingredients(state = initialState, action) {
+    if (typeof state === 'undefined') {
+        return initialState;
+    }
+    switch (action.type) {
+        case "FETCH_INGREDIENTS_PENDING": {
+            return {
+                ...state,
+                fetching: true,
+                fetched: false
+            }
+        }
+        case "FETCH_INGREDIENTS_REJECTED": {
+            return {
+                ...state,
+                fetching: false,
+                fetched: false,
+                error: action.payload
+            }
+        }
+        case "FETCH_INGREDIENTS_FULFILLED": {
+
+            let newIngredients = action.payload.filter(element => {
+                return checkIfKeyExists(element._id, state.ingredients);
+            });
+
+            const newState = {
+                ...state,
+                fetching: false,
+                fetched: true,
+                ingredients: [...state.ingredients, ...newIngredients]
+            }
+
+            return newState;
+        }
+
+        case "ADD_INGREDIENT_PENDING": {
+            return {
+                ...state,
+                fetching: true,
+                fetched: false
+            }
+        }
+        case "ADD_INGREDIENT_REJECTED": {
+            return {
+                ...state,
+                fetching: false,
+                fetched: false,
+                error: action.payload
+            }
+        }
+        case "ADD_INGREDIENT_FULFILLED": {
+
+            const newState = {
+                ...state,
+                fetching: false,
+                fetched: true,
+                ingredients: [...state.ingredients, action.payload]
+            }
+
+            return newState;
+        }
+
+        default: {
+            return state;
+        }
+    }
+}
