031a36eaa4e9cbf7aec3ef4af3cf019569c4e83c
Maikel Bakker <maikel.bakker1991@gmail.com>
2019-02-26 15:54:55 +0100

    refactor(createReducer): convert function to arrow function

--- src/helpers/createReducer.js
+++ src/helpers/createReducer.js
@@ -1,5 +1,5 @@
 export const createReducer = (reducerState, handlers) => {
-    return function reducer(state = reducerState, action) {
+    return (state = reducerState, action) => {
         if (Object.prototype.hasOwnProperty.call(handlers, action.type)) {
             return handlers[action.type](state, action);
         }
