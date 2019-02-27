4f68638354ec32f2f3ae4024c2332e288bcc68df
Maikel Bakker <maikel.bakker1991@gmail.com>
2019-02-27 12:49:32 +0100

    feat(css): remove css files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 import store from './store';
 
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import App from './App';
 
 ReactDOM.render(
     <Provider store={store}>
